86ebc5e00fa4d9fed6e42285e945add8d2ceb4d9
Schnittlauch202 <dominikalecbachmann@yahoo.com>
2025-12-10 15:00:18 +0100

    n:m relation

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -58,16 +58,34 @@ create Table Player_mutations (
     name varchar(100) Primary Key,
     mutation_effect varchar(100),
     cost int(100),
-    mutation_type varchar(100)
+    mutation_type varchar(100),
+    FOREIGN KEY (username) REFERENCES Player(username) --1:n relation between player and player_mutation -> add foreign key in n Table
 );
 
 create Table Status_effects (
     effect_name varchar(100) Primary Key,
-    effect_info varchar(100)
+    effect_info varchar(100),
+    FOREIGN KEY (username) REFERENCES Player(username) --1:n relation between player and status_effects -> add foreign key in n Table
 );
 
 create Table Cosmetics (
     skin_name varchar(100) Primary Key,
     skin_description varchar(100),
     unlock_condition varchar(100)
+);
+
+CREATE TABLE Enemy_Biome (
+    enemy_name VARCHAR(100),
+    biome_name VARCHAR(100),
+
+    -- Composite primary key to prevent duplicates
+    PRIMARY KEY (enemy_name, biome_name),
+
+    -- Foreign keys to maintain referential integrity
+    FOREIGN KEY (enemy_name) REFERENCES Enemies(enemy_name)
+        ON DELETE CASCADE --Causes table to automatically update or delete if the original tables get changed
+        ON UPDATE CASCADE,
+    FOREIGN KEY (biome_name) REFERENCES Biomes(biome_name)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
